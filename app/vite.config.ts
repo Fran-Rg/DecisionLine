5b78374753cb22dedb9acc3e0fa5fe6fac3c8099
Francois Regnoult <francois.regnoult@kpmg.co.uk>
2025-05-03 11:29:09 +0100

    let vite deal with manifest

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,7 +7,41 @@ export default defineConfig({
   base: './',
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        "short_name": "LeftRight",
+        "name": "Left Right Dilemma",
+        "icons": [
+          {
+            "src": "favicon.png",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+          },
+          {
+            "src": "logo_192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "logo_512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": 'logo_512.png',
+            "sizes": '512x512',
+            "type": 'image/png',
+            "purpose": 'any maskable',
+          },
+        ],
+        "start_url": ".",
+        "display": "standalone",
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff"
+      },
+    }),
   ],
   server: {
     allowedHosts: ['de0e-130-41-187-81.ngrok-free.app'],
